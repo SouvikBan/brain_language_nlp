c006d6e43ca94631e84f6b4033de7a227f93c5cc
SouvikBan <banerjeesouvik123@gmail.com>
2020-05-03 01:14:55 +0530

    bert and distilbert done(modified)

--- extract_nlp_features.py
+++ extract_nlp_features.py
@@ -26,12 +26,12 @@ def save_layer_representations(model_layer_dict, model_name, seq_len, save_dir):
 # model_options = ['bert','transformer_xl','elmo','use']        
 
 if __name__ == '__main__':
-    # parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
     # parser.add_argument("--nlp_model", default='bert', choices=model_options)                
-    # parser.add_argument("--sequence_length", type=int, default=1, help='length of context to provide to NLP model (default: 1)')
+    parser.add_argument("--sequence_length", type=int, default=1, help='length of context to provide to NLP model (default: 1)')
     #parser.add_argument("--output_dir", required=True, help='directory to save extracted representations to')
 
-    # args = parser.parse_args()
+    args = parser.parse_args()
     # print(args)
     
     text_array = np.load(os.getcwd() + '/data/stimuli_words.npy')
@@ -40,10 +40,11 @@ if __name__ == '__main__':
     for model_class, tokenizer_class, pretrained_weights, config_class in MODELS:
         word_ind_to_extract = -2
         nlp_features = get_model_layer_representations(model_class,config_class,tokenizer_class,pretrained_weights,args.sequence_length, text_array, remove_chars, word_ind_to_extract)
-        if not os.path.exists('./nlp_features/'+pretrained_weights):
-            os.makedirs('./nlp_features/'+pretrained_weights)          
+        output_dir = './nlp_features/'+pretrained_weights
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)          
               
-        save_layer_representations(nlp_features, args.nlp_model, args.sequence_length, args.output_dir)
+        save_layer_representations(nlp_features, pretrained_weights, args.sequence_length, output_dir)
     # if args.nlp_model == 'bert':
     #     # the index of the word for which to extract the representations (in the input "[CLS] word_1 ... word_n [SEP]")
     #     # for CLS, set to 0; for SEP set to -1; for last word set to -2
